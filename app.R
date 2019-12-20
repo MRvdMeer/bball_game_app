eb275b0b06d699786b38c5d9e0d99d85ed5cdd88
Maurits van der Meer <mistamasta@gmail.com>
2019-12-20 23:58:23 +0100

    update app

--- app.R
+++ app.R
@@ -9,39 +9,28 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Set up User Interface
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    dateInput("date", label = "Game Date"),
+    actionButton("click", "Update Predictions"),
+    textOutput("game_list"),
+    textOutput("predictions")
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Define server logic
+server <- function(input, output, session) {
+    game_date <- reactive( input$date )
+    
+    x1 <- eventReactive(input$click, {
+        "No predictions to show"
+    })
+    
+    output$game_list <- renderText({
+        paste( "No games on date", game_date() )
+    })
+    
+    output$predictions <- renderText({
+        x1()
     })
 }
 
